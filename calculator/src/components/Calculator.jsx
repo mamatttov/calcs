552b83ec2784ddd24c09ddb26516edd243b569a0
Kairatbek Mamatov <161651285+mamatttov@users.noreply.github.com>
2026-02-08 16:28:46 +0600

    feat: added some styles

--- calculator/src/components/Calculator.jsx
+++ calculator/src/components/Calculator.jsx
@@ -4,6 +4,10 @@ import { useState } from "react";
 export default function Calculator() {
   const [showResult, setShowResult] = useState(false);
   const keys = [
+    "AC",
+    "C",
+    "%",
+    "/",
     "7",
     "8",
     "9",
@@ -20,14 +24,23 @@ export default function Calculator() {
     "0",
     "EQUALS",
   ];
+
   return (
     <div className="min-w-[320px] bg-black flex flex-col gap-4 p-4 rounded-2xl">
       <div className="overflow-x-auto bg-[#141414] min-h-[100px] flex items-end justify-end  flex-col p-4 rounded-[10px]">
-        Result
+        <div
+          className={`${showResult ? "text-[1.7rem]" : "text-[1.2rem] tracking-[2px] flex-gap-[-5px] items-center text-[rgba(255,255,255,0.5)] justify-end"}`}
+        >
+          Result
+        </div>
       </div>
-      <div>
+      <div className="grid grid-cols-[repeat(4,1fr)] gap-[0.3rem]">
         {keys.map((item, index) => (
-          <Keys label={item} key={index} />
+          <Keys
+            label={item}
+            key={index}
+            keyClass={item === "EQUALS" && "equals"}
+          />
         ))}
       </div>
     </div>
--- calculator/src/components/Keys.jsx
+++ calculator/src/components/Keys.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 
 export default function Keys(props) {
-  const { label, key } = props;
+  const { label, key, keyClass } = props;
+  const equalsClass =
+    "col-[span_2] bg-orange-400 text-[#1a261a] font-semibold hover:bg-orange-500";
   return (
-    <div>
+    <div
+      className={`bg-[#141414] flex cursor-pointer items-center justify-center p-4 rounded-[5px] hover:bg-[#4ccdc742] ${keyClass && equalsClass}`}
+    >
       <button key={key}>{label}</button>
     </div>
   );
